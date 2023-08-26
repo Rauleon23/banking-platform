46dc50fe0e27d56ee577acf8f1d78f7074eda37a
Rauleon23 <129474930+Rauleon23@users.noreply.github.com>
2023-08-26 11:44:43 -0500

    Update version
    Co-authored-by: Camilo Andres Camargo Castañeda <camilocamargo49@gmail.com>

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Banking Platform version 1.0!')
+    res.send('Banking Platform version 0.0.1!')
 })
 
 app.listen(port, () => {
