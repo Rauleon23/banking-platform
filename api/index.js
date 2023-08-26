df6bcb5a551d45f8cb0eb188a435ddd7e60e0204
Rauleon23 <raufervakleo@gmail.com>
2023-08-26 11:35:29 -0500

    Change message main route

--- api/index.js
+++ api/index.js
@@ -3,13 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.get('/user', (req, res) => {
-    res.send('Raul')
+    res.send('Banking Platform version 1.0!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Listening on port http://localhost:${port}`)
 })
